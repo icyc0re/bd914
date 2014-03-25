3a0050205b36228808ee14297a9a44364833e31f
tiziano.signo <tiziano.signo@gmail.com>
2014-03-25 20:02:13 +0100

    website base

---
+++ website/fsrecommender/fsrecommender/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from visualizer.views import home
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fsrecommender.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^$', home),
+)
