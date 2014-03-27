93fdd5b11a4ab6fe2fe3f6752d4ca39c3492af34
Ivan Gavrilovic <gavra0@gmail.com>
2014-03-27 15:40:14 +0100

    refactored crawler, added list of venue ids for man&brok, and stat isl, refs #1

--- api/crawler/pull_ids.py
+++ api/crawler/pull_ids.py
@@ -23,7 +23,7 @@ def compute_new_geolocation(lat, lon, lat_offset, lon_offset):
     
     return latO, lonO
 
-def main():	
+def main(): 
     
     if len(sys.argv) != 3:
         print 'usage: pull_ids.py <login_file> <output_file>, see README'
@@ -41,52 +41,62 @@ def main():
     square_size=50.0 #square size to search in meters
     
     manhattan_brooklin={'n':40.913515,'e':-73.689612,'s':40.534680,'w':-74.039802}
-    state_island={'n':40.647827,'e':-73.702659,'s':40.526851,'w': -74.271888}
+    staten_island={'n':40.647827,'e':-73.702659,'s':40.526851,'w': -74.271888}
     full_New_York={'s':40.487693,'n':40.917923,'w':40.494484,'e':-74.040488}
     
     #start point at north east
-    latitude = manhattan_brooklin['n']
-    longitude= manhattan_brooklin['e']
+    latitude = staten_island['n']
+    longitude= staten_island['e']
+    start_longitude = staten_island['e']
+
+    min_latitude = staten_island['s']
+    min_longitude = staten_island['w']
     
     #I ALREADY PULL UP TO THAT GEOLOCATION, SO START FROM HERE
-    latitude = 40.7576572982
-    longitude= -73.8271798354
-     
+    #latitude = 40.647827
+    #longitude= -73.702659
+
     #pull venues on 50m^2 squares
     with open(sys.argv[2],'a') as outfile:
         #move vertically
-        while latitude > manhattan_brooklin['s']:
+        while latitude > min_latitude:
 
             #move search area <square_size> to south
             new_latitude,longitude=compute_new_geolocation(latitude, longitude, -square_size, 0)
+            mul_factor = 1.0
             
             #move horizontally
-            while longitude > manhattan_brooklin['w']:
+            while longitude > min_longitude:
                 #move search area <square_size> to west
-                latitude,new_longitude = compute_new_geolocation(latitude, longitude, 0, -square_size)
-                print 'pulling from area ne:' + str(latitude)+',' +str(longitude) + ', sw:' + str(new_latitude)+',' + str(new_longitude)
+                latitude,new_longitude = compute_new_geolocation(latitude, longitude, 0, -(square_size * mul_factor))
+                print 'pulling from area ne:' + str(latitude)+',' +str(longitude) + ', sw:' + str(new_latitude)+',' + str(new_longitude) + '; square size ' + str(square_size * mul_factor)
                                 
                 try:
                     search = client.venues.search(params={'sw':str(new_latitude)+','+str(new_longitude),
                                                           'ne':str(latitude)+','+str(longitude),
                                                           'limit':'50', 'intent':'browse'})
                     venues_ids = [venue["id"] for venue in search["venues"]]
-                    print '\t fetched '+str(len(venues_ids)) +' venues ids'           
+                    venues_count = len(venues_ids)
+                    print '\t fetched '+ str(venues_count) +' venues ids'
+                    if venues_count == 50 and mul_factor > 0.24:
+                        mul_factor /=2      
+                    else:
+                        mul_factor+=1
+
+                        #write ids to file
+                        outfile.write('ne:' + str(latitude)+',' +str(longitude) + ', sw:' + str(new_latitude)+',' + str(new_longitude)+'\n')
+                        for venue_id in venues_ids:
+                            print>>outfile, venue_id
+                        longitude=new_longitude
+
                 except foursquare.RateLimitExceeded:
                     print 'Rate limit exceeded...waiting 10s'
                     time.sleep(10);
-                
-                #write ids to file
-                outfile.write('ne:' + str(latitude)+',' +str(longitude) + ', sw:' + str(new_latitude)+',' + str(new_longitude)+'\n')
-                for venue_id in venues_ids:
-                    print>>outfile, venue_id
-                    
-                longitude=new_longitude
             
             latitude = new_latitude
             
             #reset longitude to east
-            longitude = manhattan_brooklin['e']
+            longitude = start_longitude
         
 if __name__ == '__main__':
     main()
