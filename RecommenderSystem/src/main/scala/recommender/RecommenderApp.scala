1999a18fcb80f65a9d985c72e8e234b2afcc14e4
Alexandru Ardelean <alexandru.ardelean@epfl.ch>
2014-04-12 15:42:19 +0200

    Created RecommenderSystem Scala project under Eclipse

---
+++ RecommenderSystem/src/main/scala/recommender/RecommenderApp.scala
@@ -0,0 +1,20 @@
+package recommender
+
+import input.MockAbstractInputProcessor
+import filtering.MockVectorSimilarity
+
+/**
+ * This is the main class of the recommender system.
+ * @author Ivan Gavrilović
+ */
+object RecommenderApp {
+  def main(args:Array[String]){
+    // todo
+    val processor:MockAbstractInputProcessor = new MockAbstractInputProcessor
+
+    val vectors = processor.processData(null)
+    println(vectors)
+
+    println(MockVectorSimilarity.calculateSimilarity(vectors(0), vectors(1)))
+  }
+}
