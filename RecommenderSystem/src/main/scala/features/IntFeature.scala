316d982a6a56b07f2a67f6f3de94d6d69333271a
Alexandru Ardelean <alexandru.ardelean@epfl.ch>
2014-04-15 18:17:09 +0200

    Added Context aware components to main Recommendation project.

--- RecommenderSystem/src/main/scala/features/IntFeature.scala
+++
@@ -1,9 +0,0 @@
-package features
-import features.Feature
-
-/**
- * Case class representing the features that have [[scala.Int]]] values
- * @author Ivan Gavrilović
- */
-case class IntFeature(key:String, value:Int) extends Feature[String, Int](key, value){
-}
