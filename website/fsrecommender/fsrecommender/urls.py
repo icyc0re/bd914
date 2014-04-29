0342a3aacc290ee4efca20b47a5ce6c701a720ec
tiziano_signo <tiziano.signo@gmail.com>
2014-04-29 19:46:01 +0200

    add oauth starting point

--- website/fsrecommender/fsrecommender/urls.py
+++ website/fsrecommender/fsrecommender/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from visualizer.views import home
+from visualizer.views import home, coldstart, login
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     (r'^$', home),
+    (r'^login/$', login),
+    (r'^coldstart/$', coldstart),
 )
--- website/fsrecommender/visualizer/views.py
+++ website/fsrecommender/visualizer/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
+	# signup - oauth
+
+	# connect to (link below)
+
+	# code maccari
+
+	# https://foursquare.com/oauth2/authenticate?client_id=SBUR2MWKKUYWPHJTZU4OLBAMFLGWKWZZJQKVGAAEJV1URHSP&response_type=token&redirect_uri=http://localhost:8000
+
+
+	# TODO: get user data
+
 	return render(request, 'home.html')
+
+
+def login(request):
+	return redirect('https://foursquare.com/oauth2/authenticate?client_id=SBUR2MWKKUYWPHJTZU4OLBAMFLGWKWZZJQKVGAAEJV1URHSP&response_type=token&redirect_uri=http://localhost:8000')
+
+
+def coldstart(request):
+	return render(request, 'start.html')
