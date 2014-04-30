51ebf8b1517a87c1884980b1edffc985c33ef8da
Bernard Maccari <bernard.maccari@epfl.ch>
2014-04-30 02:20:28 +0200

    user's json from access token

--- website/fsrecommender/visualizer/views.py
+++ website/fsrecommender/visualizer/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render, redirect
+import foursquare
+import json
+
 
 # Create your views here.
 def home(request):
 	# signup - oauth
 
 	# connect to (link below)
-
+	
 	# code maccari
-
+	# TODO: get anchor from request path
+	login_request = "#access_token=XFABOQ4PIP52UBOST0ZIVC2KKO002WPCUS4IXAV3LCPNNLUY"
 	# https://foursquare.com/oauth2/authenticate?client_id=SBUR2MWKKUYWPHJTZU4OLBAMFLGWKWZZJQKVGAAEJV1URHSP&response_type=token&redirect_uri=http://localhost:8000
 
+	if 'access_token' in login_request:
+		accessToken = login_request[login_request.find("=")+1:]
+		print accessToken
+		client = foursquare.Foursquare(access_token=accessToken)
+		user = client.users()
+		
+		jsonString = json.dumps(user) 	# as string
 
-	# TODO: get user data
+# 		userPath = "./user.json" 
+# 		with open(userPath,'a') as outfile:
+# 			json.dump(user, outfile) 	# in file
 
 	return render(request, 'home.html')
 
