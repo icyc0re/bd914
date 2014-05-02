1f36d57bb4618b27fc96b7b49798cd30b57e29f8
Boris <borisvperovic@gmail.com>
2014-05-02 22:26:58 +0200

    Reply to web app.

--- recommender_module/src/main/scala/recommender/RecommenderApp.scala
+++ recommender_module/src/main/scala/recommender/RecommenderApp.scala
@@ -2,9 +2,11 @@ package recommender
 
 import filtering.MockVectorSimilarity
 import vectors.{UserVector, VenueVector}
-import scala.collection.mutable
 import input.{Checkins, CheckinsCount}
 import java.io.File
+import weboutput.{ResponseToWebApp, Response}
+import javax.print.attribute.standard.ReferenceUriSchemesSupported
+import scalaj.http.Http
 
 /**
  * This is the main class of the recommender system.
@@ -27,9 +29,11 @@ object RecommenderApp {
     val sorted = MockVectorSimilarity.sortUserVenueSimilarities(similarities);
     MockVectorSimilarity.printTopKSimilarities(sorted, 5);
 
+    ResponseToWebApp.replyToWebApp(sorted, 3, 0);
+
     // checkins parser test
-    val file = new File("../dataset/sample/checkinstest.json")
-    val response = new Checkins(scala.io.Source.fromFile(file).mkString)
-    response.displayFeatures()
+    //val file = new File("../dataset/sample/checkinstest.json")
+    //val response = new Checkins(scala.io.Source.fromFile(file).mkString)
+    //response.displayFeatures()
   }
 }
--- recommender_module/src/main/scala/utils/Cons.scala
+++ recommender_module/src/main/scala/utils/Cons.scala
@@ -30,6 +30,11 @@ object Cons {
   val CATEGORIES_INPUT_PATH = "../dataset/sample/ordered_categories.txt"
   val CATEGORIES_MATRIX_INPUT_PATH = "../dataset/sample/categories_similarity.txt"
 
+  val WEBAPP_RESULT_URL = "http://webapp.com:80/result"
+  val WEBAPP_RESULT_PARAM_USERID = "userId"
+  val WEBAPP_RESULT_PARAM_VENUEIDS = "venueIds"
+
+
   val FEATURE_RATING = "Rating"//Name given to the feature containing the rating of a venue. This feature is added during the similarity computation phase
   
   val NY_AREA = Map[String, Double]("s"->40.526851, "n"->40.913515,"w"-> -74.271888,"e"-> -73.689612)
