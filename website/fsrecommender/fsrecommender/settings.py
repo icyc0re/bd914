8a151eeffbc564fc9e2f6c1a485cd02aafe2814b
icyc0re <tiziano.signo@gmail.com>
2014-05-13 20:03:20 +0200

    remove old references, finish merging map section

--- website/fsrecommender/fsrecommender/settings.py
+++ website/fsrecommender/fsrecommender/settings.py
@@ -95,12 +95,10 @@ STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
-    os.path.join(BASE_DIR, 'map-draft'),
 )
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'map-draft'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
