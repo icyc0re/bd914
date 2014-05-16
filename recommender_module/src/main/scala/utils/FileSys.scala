b6c499195140bceca2f596f486da6f676e25ee8a
Ivan Gavrilovic <gavra0@gmail.com>
2014-05-16 14:43:26 +0200

    add file operation utils

---
+++ recommender_module/src/main/scala/utils/FileSys.scala
@@ -0,0 +1,32 @@
+package utils
+
+import java.io.{FileInputStream, File}
+
+/**
+ * @author Ivan Gavrilović
+ */
+object FileSys {
+  def readFile(path: String): String = {
+    try {
+      val is = new FileInputStream(path)
+      val content = scala.io.Source.fromInputStream(is)(scala.io.Codec.UTF8).mkString
+      is.close()
+      content
+    }
+    catch {
+      case e: Exception => println("Path = " + path + " - " + e); ""
+    }
+  }
+
+  def getLines(path: String): Seq[String] = {
+    readFile(path).split("\n").toSeq
+  }
+
+  def readDir(path: String): Seq[String] = {
+    val dir = new File(path)
+    if (!dir.isDirectory) {
+      throw new Exception("Directory expected")
+    }
+    dir.listFiles.filter((x: File) => !x.getName.startsWith(".") && x.isFile).map(_.getAbsolutePath)
+  }
+}
