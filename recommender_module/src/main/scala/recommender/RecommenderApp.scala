d8e2757efbf2af98b5870d6250e82e67d573e615
Ivan Gavrilovic <gavra0@gmail.com>
2014-05-19 23:37:20 +0200

    spark 100 users

--- recommender_module/src/main/scala/recommender/RecommenderApp.scala
+++ recommender_module/src/main/scala/recommender/RecommenderApp.scala
@@ -78,7 +78,7 @@ object RecommenderApp {
 
     val venuesWholeList: Seq[String] = FileSys.readDir(Cons.VENUES_PATH)
     val venuesList: Seq[Seq[String]] = splitThirds(venuesWholeList)
-    val userWholeList: Seq[String] = FileSys.readDir(Cons.USERS_PATH).take(3)
+    val userWholeList: Seq[String] = FileSys.readDir(Cons.USERS_PATH).take(100)
     val userList: Seq[Seq[String]] = List(userWholeList.slice(0, 33), userWholeList.slice(33, 67), userWholeList.slice(67, 100)) //splitThirds(userWholeList)
     val userVenues = sc.makeRDD(venuesList, 4).cartesian[Seq[String]](sc.makeRDD(userList, 3))
 
