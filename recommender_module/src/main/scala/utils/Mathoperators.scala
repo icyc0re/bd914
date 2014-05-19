ef8b6e881a9f8de45accbe316d2214a1f1e764a5
sprlye <nice.mourning@gmail.com>
2014-05-19 23:00:28 +0200

    Solves Mathoperators

--- recommender_module/src/main/scala/utils/Mathoperators.scala
+++ recommender_module/src/main/scala/utils/Mathoperators.scala
@@ -8,17 +8,34 @@ object Mathoperators {
   def inclusion(timeInt1: ((Int, Int, Int), (Int, Int, Int)), timeInt2: ((Int, Int, Int), (Int, Int, Int))): Boolean = {
     //return true is timeInt2 is included in timeInt1
     var included = false
-    if ((timeInt1._1._1 < timeInt2._1._1)
-        || ((timeInt1._1._1 == timeInt2._1._1) && (timeInt1._1._2 < timeInt2._1._2))
-        || ((timeInt1._1._1 == timeInt2._1._1) && (timeInt1._1._2 == timeInt2._1._2) && (timeInt1._1._3 <= timeInt2._1._3))
+
+    //Check if the intervals are real intervals
+    if ((timeInt1._1._1 < timeInt1._2._1)
+      || ((timeInt1._1._1 == timeInt1._2._1) && (timeInt1._1._2 < timeInt1._2._2))
+      || ((timeInt1._1._1 == timeInt1._2._1) && (timeInt1._1._2 == timeInt1._2._2) && (timeInt1._1._3 <= timeInt1._2._3))
+    ) {
+
+      if ((timeInt2._1._1 < timeInt2._2._1)
+        || ((timeInt2._1._1 == timeInt2._2._1) && (timeInt2._1._2 < timeInt2._2._2))
+        || ((timeInt2._1._1 == timeInt2._2._1) && (timeInt2._1._2 == timeInt2._2._2) && (timeInt2._1._3 <= timeInt2._2._3))
+      ) {
+
+        if ((timeInt1._1._1 < timeInt2._1._1)
+          || ((timeInt1._1._1 == timeInt2._1._1) && (timeInt1._1._2 < timeInt2._1._2))
+          || ((timeInt1._1._1 == timeInt2._1._1) && (timeInt1._1._2 == timeInt2._1._2) && (timeInt1._1._3 <= timeInt2._1._3))
         ) {
-      if ((timeInt1._2._1 > timeInt2._2._1) 
-          || ((timeInt1._2._1 == timeInt2._2._1) && (timeInt1._2._2 > timeInt2._2._2))
-          || ((timeInt1._2._1 == timeInt2._2._1) && (timeInt1._2._2 == timeInt2._2._2) && (timeInt1._2._3 >= timeInt2._2._3))
+          if ((timeInt1._2._1 > timeInt2._2._1)
+            || ((timeInt1._2._1 == timeInt2._2._1) && (timeInt1._2._2 > timeInt2._2._2))
+            || ((timeInt1._2._1 == timeInt2._2._1) && (timeInt1._2._2 == timeInt2._2._2) && (timeInt1._2._3 >= timeInt2._2._3))
           ) {
-        included = true
+            included = true
+          }
+        }
       }
     }
+
+
+
     included
   }
   
