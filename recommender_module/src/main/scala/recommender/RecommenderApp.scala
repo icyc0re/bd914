3482f53475bad7b8b6ff153e212b78e729da2413
Ivan Gavrilovic <gavra0@gmail.com>
2014-05-20 01:12:41 +0200

    1000 users

--- recommender_module/src/main/scala/recommender/RecommenderApp.scala
+++ recommender_module/src/main/scala/recommender/RecommenderApp.scala
@@ -78,8 +78,8 @@ object RecommenderApp {
 
     val venuesWholeList: Seq[String] = FileSys.readDir(Cons.VENUES_PATH)
     val venuesList: Seq[Seq[String]] = splitThirds(venuesWholeList)
-    val userWholeList: Seq[String] = FileSys.readDir(Cons.USERS_PATH).take(100)
-    val userList: Seq[Seq[String]] = List(userWholeList.slice(0, 33), userWholeList.slice(33, 67), userWholeList.slice(67, 100)) //splitThirds(userWholeList)
+    val userWholeList: Seq[String] = FileSys.readDir(Cons.USERS_PATH).take(1000)
+    val userList: Seq[Seq[String]] = List(userWholeList.slice(0, 333), userWholeList.slice(333, 667), userWholeList.slice(667, 1000)) //splitThirds(userWholeList)
     val userVenues = sc.makeRDD(venuesList, 4).cartesian[Seq[String]](sc.makeRDD(userList, 3))
 
     println("Sending out all pairs")
